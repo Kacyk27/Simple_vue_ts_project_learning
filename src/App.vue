f33eb9d4af7d16349fa157abd65856085b8ebe77
Kacyk27 <86022445+Kacyk27@users.noreply.github.com>
2024-02-04 13:04:08 +0100

    Added completed simply project

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Some Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs, ref } from 'vue';
+import Job from './types/Job'
+import JobList from './components/JobList.vue';
+import OrderTerm from './types/OrderTerm'
+
+export default defineComponent({
+  name: 'App',
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'FarmTown', salary: 3000, id: '1'},
+      { title: 'musician', location: 'BandsTown', salary: 2800, id: '2'},
+      { title: 'Fisherman', location: 'FishTown', salary: 3500, id: '3'},
+      { title: 'Bank worker', location: 'BankTown', salary: 4000, id: '4'}
+    ])
+
+    const order = ref<OrderTerm>('title')
+    
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return {jobs, handleClick, order }
+  },
+});
+</script>
+
+<style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
+</style>
